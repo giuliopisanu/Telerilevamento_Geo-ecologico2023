2339a2e5f1931333499eeada782e6159e4ac19a4
giuliopisanu <127427310+giuliopisanu@users.noreply.github.com>
2023-04-21 17:49:56 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -5,7 +5,7 @@ terra 1.6.41
  plot(gc)
  gcc<-crop("dolansprings_oli_2013088_canyon_lrg.jpg")
  singlenr <- values(gc)
- kcluster <- kmeans(singlenr, centers = 3)
+  kcluster <- kmeans(singlenr, centers = 3)
  #1 volcanic rocks ; 2 sandstone; 3 conglomerates
  gcclass <- setValues(gc[[1]], kcluster$cluster)
  plot(gcclass)
@@ -13,8 +13,8 @@ terra 1.6.41
  frequencies
  total<- ncell(gcclass)
  #percentuale corrispondente al n di pixel per ogni singola classe sul totale  sul totale
-> percentages<- frequencies*100/total
-> percentages
+ percentages<- frequencies*100/total
+ percentages
 # install ggplot2 packages
 
 
