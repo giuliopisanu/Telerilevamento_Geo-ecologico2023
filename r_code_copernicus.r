05d3892b550b5c3145d9d37cbf98cc449b57322b
giuliopisanu <127427310+giuliopisanu@users.noreply.github.com>
2023-05-12 17:33:09 +0200

    Update r_code_copernicus.r

--- r_code_copernicus.r
+++ r_code_copernicus.r
@@ -9,4 +9,29 @@ setwd("C:/Lab_Telerilevamento/")
 sc <- rast("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
 #rast carica due laier 1 l'immagine  2 l'incertezza (plottare il primo livello)
 plot(sc[[1]])
+scd <-as.data.frame(sc, xy= T)
+ggplot()+
+geom_raster(scd, mapping = aes(x=x,y=y, fill=ssm))+
+ggtitle("Surface soil moisture")
+#cropping an immage
+ext <- c(23, 30, 62, 68)
+sc_crop <-crop(sc, ext)
+plot(sc_crop[[1]])
+
+#plottiamo l'immagine ritagliata
+scd_crop <-as.data.frame(sc_crop, xy= T)
+ggplot()+
+geom_raster(scd_crop, mapping = aes(x=x,y=y, fill=ssm))+
+ggtitle("Surface soil moisture _cropped img")+
+scale_fill_viridis(option="cvidis")
+
+ggplot()+
+geom_raster(scd_crop, mapping = aes(x=x,y=y, fill=ssm))+
+ggtitle("Surface soil moisture _cropped img")+
+scale_fill_viridis(option="viridis")
+
+
+
+
+
 
