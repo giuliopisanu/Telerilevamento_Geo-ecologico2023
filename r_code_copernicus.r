6105a0a02870dd5a719afbccbdb9cf42aa9e89cf
giuliopisanu <127427310+giuliopisanu@users.noreply.github.com>
2023-05-12 16:45:35 +0200

    Update r_code_copernicus.r

--- r_code_copernicus.r
+++ r_code_copernicus.r
@@ -6,3 +6,7 @@ library(ggplot2)
 library(viridis)
 setwd("C:/Lab_Telerilevamento/")
 # dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
+sc <- rast("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
+#rast carica due laier 1 l'immagine  2 l'incertezza (plottare il primo livello)
+plot(sc[[1]])
+
